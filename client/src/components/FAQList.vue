fd33d441f8ad43364053f8f7857a92c6001712db
Peter Pallen <peter@ppallen.be>
2020-11-07 22:30:16 +0100

    Add FAQList component

---
+++ client/src/components/FAQList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="card mt-3" v-for="faq in faqs" :key="faq._id">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ faq.question }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          {{ faq.answer }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const faqs = ref([]);
+
+    const API_URL = 'http://localhost:4242/api/v1/faqs';
+
+    async function getFAQs() {
+      const response = await fetch(API_URL);
+      const json = await response.json();
+      faqs.value = json;
+    }
+
+    getFAQs();
+
+    return {
+      faqs,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
