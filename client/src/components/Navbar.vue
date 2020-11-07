2dfcaf47b0bb597ebf26ff1cffaf21e5f778f812
Peter Pallen <peter@ppallen.be>
2020-11-07 22:29:52 +0100

    Add Navbar component

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        FAQs
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
