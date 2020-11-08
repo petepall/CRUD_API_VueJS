9b23b7125819bb96127e0b9c9d1f903bd3d47295
Peter Pallen <peter@ppallen.be>
2020-11-08 23:05:04 +0100

    installed font awsome icons package

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import '@fortawesome/fontawesome-free/css/all.css';
+// eslint-disable-next-line import/extensions
+import '@fortawesome/fontawesome-free/js/all.js';
+
 import 'bulma/css/bulma.css';
 
 createApp(App)
