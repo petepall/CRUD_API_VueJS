60a186ae8aa4915bff70cfa19c282b05a7b0dcb2
Peter Pallen <peter@ppallen.be>
2020-11-11 10:11:13 +0100

    Corrected link to home

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="/">
+      <a class="navbar-item" href="#/">
         FAQs
       </a>
     </div>
